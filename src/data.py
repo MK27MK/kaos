17ccee95923308f4b8c78b5ca0d7fcf653024f10
MK27MK <mcisartist@gmail.com>
2025-04-13 13:55:19 +0200

    initial commit

---
+++ src/data.py
@@ -0,0 +1,7 @@
+from pathlib import Path
+
+
+def load_data(path: str) -> None:
+    # path can be a directory or a simple file, starting from basic csvs,
+    # they will be converted to parquet
+    pass
